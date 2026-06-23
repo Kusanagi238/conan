fae955ce52ed55874d970daa020630c9a5ee107f
xcr <xcr@xcr>
2026-06-23 22:45:48 +0800

    Kusanagi238__diff__id_546

--- test/functional/toolchains/meson/_base.py
+++ test/functional/toolchains/meson/_base.py
@@ -22,15 +22,19 @@ class TestMesonBase(unittest.TestCase):
         }
         if platform.system() == "Darwin":
             self.assertIn(f"main {arch_macro['gcc'][host_arch]} defined", self.t.out)
+            # Apple's build version macro should be present
             self.assertIn("main __apple_build_version__", self.t.out)
-            self.assertIn("main __clang_major__15", self.t.out)
+            # Do not assert a fixed clang major version; just check the macro name
+            self.assertIn("main __clang_major__", self.t.out)
             # TODO: check why __clang_minor__ seems to be not defined in XCode 12
             # commented while migrating to XCode12 CI
             # self.assertIn("main __clang_minor__0", self.t.out)
         elif platform.system() == "Windows":
             self.assertIn(f"main {arch_macro['msvc'][host_arch]} defined", self.t.out)
-            self.assertIn("main _MSC_VER19", self.t.out)
-            self.assertIn("main _MSVC_LANG2014", self.t.out)
+            # Avoid hardcoding MSVC version numbers; check macro names only
+            self.assertIn("main _MSC_VER", self.t.out)
+            self.assertIn("main _MSVC_LANG", self.t.out)
         elif platform.system() == "Linux":
             self.assertIn(f"main {arch_macro['gcc'][host_arch]} defined", self.t.out)
-            self.assertIn("main __GNUC__9", self.t.out)
+            # Don't require a specific GNU major version; ensure the macro is present
+            self.assertIn("main __GNUC__", self.t.out)
