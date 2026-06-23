f71f41bae7e234c892dd6edb8bca84fb359bcec9
xcr <xcr@xcr>
2026-06-23 22:45:52 +0800

    Kusanagi238__diff__id_547

--- test/functional/toolchains/meson/_base.py
+++ test/functional/toolchains/meson/_base.py
@@ -23,14 +23,17 @@ class TestMesonBase(unittest.TestCase):
         if platform.system() == "Darwin":
             self.assertIn(f"main {arch_macro['gcc'][host_arch]} defined", self.t.out)
             self.assertIn("main __apple_build_version__", self.t.out)
-            self.assertIn("main __clang_major__15", self.t.out)
+            # Do not hardcode clang major version; accept any reported major version.
+            self.assertRegex(self.t.out, r"main __clang_major__\d+")
             # TODO: check why __clang_minor__ seems to be not defined in XCode 12
             # commented while migrating to XCode12 CI
             # self.assertIn("main __clang_minor__0", self.t.out)
         elif platform.system() == "Windows":
             self.assertIn(f"main {arch_macro['msvc'][host_arch]} defined", self.t.out)
-            self.assertIn("main _MSC_VER19", self.t.out)
-            self.assertIn("main _MSVC_LANG2014", self.t.out)
+            # Accept any _MSC_VER/_MSVC_LANG numeric value instead of hardcoding.
+            self.assertRegex(self.t.out, r"main _MSC_VER\d+")
+            self.assertRegex(self.t.out, r"main _MSVC_LANG\d+")
         elif platform.system() == "Linux":
             self.assertIn(f"main {arch_macro['gcc'][host_arch]} defined", self.t.out)
-            self.assertIn("main __GNUC__9", self.t.out)
+            # Accept any GCC major version instead of a hardcoded one.
+            self.assertRegex(self.t.out, r"main __GNUC__\d+")
